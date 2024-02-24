82fa81a9bd7a4e307edc19687e8226ec567c5e08
Raprap29 <maglayaralph29@gmail.com>
2024-02-24 16:18:41 +0800

    Update System

--- app/components/SideAds/SideBarAds.tsx
+++ app/components/SideAds/SideBarAds.tsx
@@ -18,10 +18,13 @@ const SideBarAds: React.FC = () => {
         <React.Fragment>
             <ShowAdsBox setshowAds={setshowAds} showAds={showAds} />
             <div className="fixed z-[700] top-[50%] left-0">
-                <button onClick={handleShowAds} type="button" className="bg-[#000] h-[240px] flex flex-col gap-y-[10px] rounded-[5px] shadow-3dshadow text-white p-1">
+                <button onClick={handleShowAds} type="button" className="relative bg-[#000] h-[220px] flex flex-col gap-y-[10px] rounded-[5px] shadow-3dshadow text-white p-1">
                     {paths.map((path: any, index: number) => (
                         <Image key={index} src={`/icon/${path}.png`} width={30} height={5} alt="icon" />
                     ))}
+                    <div className="absolute rotate-90 top-[132px] left-[-40px]">
+                        <p className="text-white whitespace-nowrap">FOLLOW ON US</p>
+                    </div>
                 </button>
             </div>
         </React.Fragment>
