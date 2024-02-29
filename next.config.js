1d02f56ff63868187b59d21c5d1f474e49e74b0e
unknown <Raprap29>
2024-02-28 18:49:00 -0800

    Update System

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     // unoptimized: true,
+    env: {
+        NEXT_PUBLIC_API: 'https://serverjama.vercel.app'
+    }
 }
 
 module.exports = nextConfig
