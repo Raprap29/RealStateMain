781869cba0cef954389ead4848f85ca8f1d2f99f
Admin <Admin>
2024-07-22 21:20:46 +0800

    Upda

--- app/Product.tsx
+++ app/Product.tsx
@@ -33,7 +33,7 @@ export default function Product(props: any){
     
     return(
         <>
-            <div key={props.id} className="relative rounded-[5px] bg-[#fff] flex flex-col items-center shadow-[0px_0px_3px_2px_rgba(74,107,222,.25)] max-[360px]:w-[220px] w-[260px]">
+            <div key={props.id} className="relative rounded-[5px] bg-[#fff] flex flex-col items-center shadow-[0px_0px_3px_2px_rgba(74,107,222,.25)] max-[360px]:w-[220px]  w-full">
                 <div className="p-2 relative">
                     <Image src={props.isImage} alt="image" width={240} height={180} className="rounded-[5px]" />
                     <div className="absolute top-[17px] left-[22px] flex gap-x-[30px]">
@@ -47,8 +47,8 @@ export default function Product(props: any){
                         </div>
                     </div>
                 </div>
-                <div className="mt-1 w-full">
-                        <p className="max-[360px]:ml-10 max-[360px]:mr-10 ml-[50px] mt-[4px] text-[14px] font-medium max-[360px]:text-[10px]">{handleChangeContent(props.content)}</p>
+                <div className="mt-1">
+                        <p className="max-[360px]:ml-10 max-[360px]:mr-10 mr-[50px] ml-[50px] mt-[4px] text-[14px] font-medium max-[360px]:text-[10px]">{handleChangeContent(props.content)}</p>
                         <div className="flex text-[15px] justify-between pr-[10px]  pl-[10px] mt-2 ml-10 mr-10">
                         <div className="text-[12px] max-[400px]:text-[10px]">
                             <p className="font-light pt-1 pb-1">Property Type: <span className="font-medium">{props.type}</span></p>
@@ -73,10 +73,10 @@ export default function Product(props: any){
                             <div className={`bg-[#000] h-[2px] w-[118px] max-[390px]:w-[95px]`} style={{background: "repeating-linear-gradient(90deg,rgba(0,0,0,0.50) 0 4px ,#0000 0 8px) center", backgroundSize: "100% 2px"}}></div>
                         </div>
                     </div>
-                    <div className="flex justify-center">
-                        <div className="flex justify-center items-center w-full pl-2 pr-2  max-[360px]:pl-[30px] max-[360px]:pr-[30px]">
-                            <Link href={`/jama_property/${props._id}`} className="rounded-[5px] text-white bg-[#18872B] px-5 py-[8px]">View Details</Link>
-                        </div>
+                </div>
+                <div className="flex justify-center">
+                    <div className="flex justify-center items-center w-full pl-2 pr-2  max-[360px]:pl-[30px] max-[360px]:pr-[30px]">
+                        <Link href={`/jama_property/${props._id}`} className="rounded-[5px] text-white bg-[#18872B] px-5 py-[8px]">View Details</Link>
                     </div>
                 </div>
             </div>
